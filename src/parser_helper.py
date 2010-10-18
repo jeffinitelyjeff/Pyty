4d09dad8de644afbc77d1c99d2ea4b8ea72338ef
Jeffrey Ruberg <jruberg@wesleyan.edu>
2010-10-18 04:13:13 -0400

    realized parsing the file with epydoc would be a really big pain and that it'll probably be easier to do with ast

--- src/parser_helper.py
+++ src/parser_helper.py
@@ -1,48 +1,20 @@
-def get_namespace_contents(doc):
-    """Returns the contents present in the C{doc}'s namespace, encoded as a
-    dictionary mapping identifiers (strs) to L{epydoc.apidoc.VariableDoc}s. 
-    That is, C{get_namespace_contents} called on a module will only return
-    information about module-level variables, functions, and classes.
+import compiler.parseFile
+import ast.NodeVisitor
 
-    @type doc: L{epydoc.apidoc.APIDoc}.
-    @param doc: Representation of API documentation for a Python object (which
-        can be a module, class, etc.).
-    @rtype: dict {str : C{VariableDoc}}.
-    @return: Contents of C{doc}'s namespace.
+class PyTyNodeVisitor(ast.NodeVisitor):
+
+    def visit(node):
+        
+
+def get_ast(filename):
+    """Returns the Abstract Syntax Tree for the Python code in file 
+    C{filename}.
+
+    @type filename: C{str}.
+    @param filename: A valid path to a Python source code file.
+    @rtype: an AST.
+    @return: An Abstract Syntax Tree for the code in the file if it is a valid
+    Python source code file, none if it isn't.
     """
 
-    return api_doc.variables
-
-
-def get_all_objects(doc):
-    """Recursively gathers API documentation on all objects present in C{doc},
-    not just those directly in C{doc}'s namespace. Returns all the contents of
-    C{doc}, encoded as a dictionary mapping identifiers (strs) to
-    L{epydoc.apidoc.VariableDoc}s.
-
-    @type doc: L{epydoc.apidoc.APIDoc}.
-    @param doc: Representation of API documentation for a Python object (which
-        can be a module, class, etc.).
-    @rtype: dict {C{str} : C{VariableDoc}}.
-    @return: All contents of C{doc}.
-    """
-
-    return get_all_objects_helper({}, doc)
-
-
-def get_all_objects_helper(d, doc):
-    """Helper function for L{get_all_objects}. Passes along a dictionary to
-    store to incrementally store variable mappings.
-
-    @type d: dict {C{str} : C{VariableDoc}}.
-    @param d: Storage dictionary. Initially stores all previous contents that
-        have been found.
-    @rtype: dict {C{str} : C{VariableDoc}}.
-    @return: Updates C{d} to add contents found in the namespace of C{doc}, as
-        well as all in the namespace of all of C{doc}'s children.
-    """
-
-    namespace_contents = get_namespace_contents(doc)
-    d.update(namespace_contents)
-
-    return get_all_objects_helper(d, doc)
+    return compiler.parseFile(filename)
