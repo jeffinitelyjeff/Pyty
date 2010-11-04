4195554be2d7df530ad4666ad5dbdd6590d64ef2
Jeff Ruberg <jruberg@wesleyan.edu>
2010-11-04 11:29:54 -0400

    added DoesNotTypecheck exception

--- src/typer.py
+++ src/typer.py
@@ -1,3 +1,5 @@
+# This class is mostly being written now as a skeleton of what it should do 
+
 class UnsatisfactoryEnvironment(Exception):
     """Exception subclass which contains data specific to an environment which
     was found to be unsatisfactory in determining whether an AST typechecks
@@ -5,28 +7,54 @@ class UnsatisfactoryEnvironment(Exception):
     
     @type env: an environment, specifically C{dict} {C{str} : C{str}}.
     @ivar env: the environment found to be unsatisfactory in typechecking.
-    @type req_types: C{list} [C{str}].
-    @ivar req_types: a list of variable names which must have types in order for
-        the AST to be properly typechecked.
+    @type req_types: C{str}.
+    @ivar req_type: the variable with missing type information that was
+        necessary in order to typecheck.
     """
 
-    def __init__(self, env, req_types): 
+    def __init__(self, env, req_type): 
         self.env = env
-        self.req_types = req_types
+        self.req_type = req_type
 
     def __str__(self):
-        return "Unsatisfactory Environment:\n" + repr(self.env) + "\n\n \
-                Required Types:\n" + repr(self.req_types)
+        return "%s:\n%r\n\n%s\n%r" % ("Unsatisfactory Environment", self.env,
+            "Required Type", self.req_type)
+
+class DoesNotTypecheck(Exception):
+    """Exception subclass which contains data specific to an AST which does
+    not typecheck properly.
+
+    @type tree: an AST node.
+    @ivar tree: the AST which does not typecheck.
+    @type node: an AST node.
+    @ivar node: the AST node in C{tree} which does not typecheck.
+    @type node_env: an environment, specifically C{dict} {C{str] : C{str}}
+    @ivar node_env: the environment at c{node}.
+    """
+
+    def __init__(self, tree, node, node_env):
+        self.tree = tree
+        self.node = node
+        self.node_env = node_env
+
+    def __str__(self):
+        return "%s:\n%r\n\n%s\n%r\n\n%s\n%r" % ("Full Tree", self.tree, "Error
+            Tree", self.node, "Environment of Error Tree", self.node_env)
+
 
 def typecheck(tree, env):
-    """Determines if an AST C{tree} typechecks with environment C{env}. Throws
-    an UnsatisfactoryEnvironment exception if the C{env} doesn't contain enough
-    type information to typecheck C{tree}.
+    """Returns the type of an AST C{tree} with environment C{env}, assuming it
+    typechecks properly.
 
     @type tree: an AST node.
     @param tree: an AST to typecheck.
-    @type env: C{dict} {C{str} : C{str}}.
+    @type env: c{dict} {C{str} : C{str]].
     @param env: an environment mapping variable names to types.
+    @rtype: {str}.
+    @return: the type of C{tree}.
+    @raise UnsatisfactoryEnvironment: if the C{env} doesn't contain enough
+        type information to determine the type of C{tree}.
+    @raise DoesNotTypehcheck: if something doesn't typecheck within C{tree}.
     """
 
     # TODO: Implement function
