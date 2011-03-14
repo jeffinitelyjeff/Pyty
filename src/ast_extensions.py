7777ef107ad39daa7d4943c7efdb37e0abdd91df
Jeff Ruberg <jruberg@wesleyan.edu>
2011-03-14 00:21:57 -0400

    cleaned up ast_extensions and put things into reasonable classes (a bunch of helper functions was bleh)

--- src/ast_extensions.py
+++ src/ast_extensions.py
@@ -2,40 +2,72 @@ import ast
 
 from util import are_disjoint, disjoint_sums_of
 
-### These are classifications of statement types essentially based on whether
-### the statement is multi-line or not. All statement types in
-### _SIMPLE_STATEMENTS do not have lists of statements as children, but every
-### statement type in _COMPOUND_STATEMENT does have at least one list of
-### statements as a child.
+class ASTInfo:
+    """
+    Class to encapsulate and provide info about the AST.
 
-_SIMPLE_STATEMENTS = set([ "Assign", "Return", "Delet", "AugAssign", "Raise",
-                           "Assert", "Import", "ImportFrom", "Print", "Pass",
-                           "Break", "Continue" ])
-_COMPOUND_STATEMENTS = set([ "If", "While", "FunctionDef", "ClassDef", "For",
-                             "With", "TryExcept", "TryFinally" ])
-_ALL_STATEMENTS = _SIMPLE_STATEMENTS.union(_COMPOUND_STATEMENTS)
+    @type simple_stmts: C{set} of C{str}
+    @cvar simple_stmts: Statement types that do not contain other statements as
+        children.
+    @type compound_stmts: C{set} of C{str}
+    @cvar compound_stmts: Statement types that do contain other statements as
+        children.
+    @type all_stmts: C{set} of C{str}
+    @cvar all_stmts All statement types.
+    @type body_stmts: C{set} of C{str}
+    @cvar body_stmts: Compound statement types that contain statement children
+        in one list labeled 'body'.
+    @type body_orelse_stmts: C{set} of C{str}
+    @cvar body_orelse_stmts: Compound statement types that contain statement
+        children in one list labeled 'body' and another list labeled 'orelse'.
+    @type body_finally_stmts: C{set} of C{str}
+    @cvar body_finally_stmts: Compound statement types that contain statement
+        children in one list labeled 'body' and another list labeled 'finally'.
+    """
 
-# simple and compound statement should not overlap.
-assert(_are_disjoint(_SIMPLE_STATEMENTS, _COMPOUND_STATEMENTS)
+    simple_stmts = set(("Assign Return Delete AugAssign Raise Assert " + \
+                        "Import ImportFrom Print Pass Break Continue").split())
+    compound_stmts = set(("If While FunctionDef ClassDef For With " + \
+                          "TryExcept TryFinally").split())
+    all_stmts = simple_stmts.union(compound_stmts)
 
-### These are classifications of compound statements based on the structure of
-### their corresponding AST node. Body statements contain only one list of
-### statements labeled "body", body-orelse statements contain lists of
-### statements labeled "body" and "orelse", and body-finally statements contain
-### lists of statements labeled "body" and "finally".
+    # simple and compound statements should not overlap.
+    assert(are_disjoint(simple_statements, compound_statements))
 
-_BODY_STATEMENTS = set([ "FunctionDef", "ClassDef", "With" ])
-_BODY_ORELSE_STATEMENTS = set([ "If", "While", "For", "TryExcept" ])
-_BODY_FINALLY_STATEMENTS = set([ "TryFinally" ])
 
-# make sure the complex statements are a disjoint sum of these statements.
-assert(_disjoint_sums_of([_BODY_STATEMENTS, _BODY_ORELSE_STATEMENTS,
-                          _BODY_FINALLY_STATEMENTS], _COMPOUND_STATEMENTS)
+    body_stmts = set("FunctionDef ClassDef With".split())
+    body_orelse_stmts = set("If While For TryExcept".split())
+    body_finally_stmts = set("TryFinally".split())
+    
+    # make sure the compound statements are a disjoint sum of these statements.
+    assert(disjoint_sums_of([body_stmts, body_orelse_stmts, body_finally_stmts],
+                            compound_stmts))
+
+    @staticmethod
+    def get_stmt_lists(node):
+        """Returns a tuple of the stmt lists that are children of this AST
+        C{node}. Provided so clients don't need to ask what kind of compound
+        statement C{node} is.
+        """
+    
+        node_type = node.__class__.__name__
+
+        if node_type in _BODY_ORELSE_STATEMENTS:
+            return (node.body, node.orelse)
+        elif node_type in _BODY_STATEMENTS:
+            return (node.body)
+        elif node_type in _BODY_FINALLY_STATEMENTS:
+            return (node.body, node.finalbody)
+        else:
+            # This should only be called on compound statements, which the three
+            # previous cases form a disjoint sum of.
+            assert(False)
+
 
 ### ----------------------------------------------------------------------------
 ### TypeDec class and methods to add typedec to an AST -------------------------
 ### ----------------------------------------------------------------------------
-
+           
 class TypeDec(ast.stmt):
     """A TypeDec is an AST statement node which represents the assertion that a
     variable or function is of a certain type. This is effectively a
@@ -69,6 +101,10 @@ class TypeDec(ast.stmt):
         self.t = t
         self.lineno = line
         self.col = col
+
+    @staticmethod
+    def is_typedec(node):
+        return node.__class__.__name__ == "TypeDec"
 
     def place_in_module(self, mod):
         """Places this L{TypeDec} instance in its proper place (according to its
@@ -144,137 +180,217 @@ class TypeDec(ast.stmt):
             # this default case should never be reached.
             assert(False)
 
+def TypeDecASTModule:
+    """A wrapper for an C{ast.Module} which has the property that all of its
+    children nodes have been populated with additional nodes (not specified in
+    the standard AST library) to represent Pyty type declarations.
+
+    @type tree: C{ast.Module}
+    @ivar tree: The AST backing this L{TypeDecASTModule}, which has been modified to
+        include the L{TypeDec} nodes.
+    @type typedecs: C{list} of L{TypeDec}
+    @ivar typedecs: The L{TypeDec} type declarations that have been added to
+        this AST.
+    """
+
+    def __init__(self, untyped_tree, typedecs, clone=False):
+        if clone:
+            self.original_tree = untyped_tree
+            self.tree = copy.deepcopy(untyped_tree)
+        else:
+            self.tree = untyped_tree
+
+        self.clone = clone
+        self.typedecs = typedecs
+
+        for typedec in typedecs:
+            self.place_typedec(typedec)
+
+    def __str__(self):
+        return str(self.tree) + " with " + str(self.typedecs)
+
+    def get_original_tree(self):
+        """Returns the original tree if this L{TypeDecAST} was initialized as a
+        copy (with the clone flag), and returns C{None} otherwise.
+        """
+        
+        if clone:
+            return self.original_tree
+        else:
+            return None
+
+    def place_typedec(self, typedec):
+        """Places the specified L{TypeDec} in this L{TypeDecAST}. This is
+        essentially a wrapper for the L{TypeDec} method which places the typedec
+        in a specified AST.
+        """
+        
+        typedec.place_in_module(self.tree)
+        
 ### ----------------------------------------------------------------------------
 ### Functions to add environment info to an AST populated with TypeDec nodes ---
 ### ----------------------------------------------------------------------------
-            
-def embed_environments(mod):
-    """Reads in a module AST and, for each statement, adds an instance variable
-    called C{env} which is the environment of type declarations at that current
-    state of the program. C{env} is stored as a dictionary mapping variable
-    identifiers to PytyType objects.
+
+def EnvASTModule(TypeDecASTModule):
+    """A L{TypeDecASTModule} which has the property that each statement node
+    contained in the wrapped AST has an additional field to represent the type
+    environment for the execution of that statement. The environments are stored
+    as dictionaries called C{env}, and are mappings from variable identifiers to
+    L{PytyType} objects.
+
+    @type tree: C{ast.Module}
+    @ivar tree: The AST backing this L{EnvASTModule}, which has been modified to
+        include the type environments.
     """
 
-    is_mod = mod.__class__.__name__ == "Module"
-
-    assert(is_mod)
-
-    if is_mod:
-        return _embed_environment_stmt_list(mod.body, {})
-    else:
-        return None
-    
-
-def _get_stmt_lists(node):
-    """Provides a layer of abstraction for extracting all the children from a
-    node that are lists of statements."""
-
-    node_type = node.__class__.__name__
-
-    if node_type in _BODY_ORELSE_STATEMENTS:
-        return (node.body, node.orelse)
-    elif node_type in _BODY_STATEMENTS:
-        return (node.body)
-    elif node_type in _BODY_FINALLY_STATEMENTS:
-        return (node.body, node.finalbody)
-    else:
-        # This should only be called on compound statements, which the three
-        # previous cases form a disjoint sum of..
-        assert(False)
-
-
-def _embed_environment_node(tree, old_env):
-    """Recursive helper for embed_environment to add environments to AST nodes.
-    There are three distinct types of nodes that this can be called on:
-    expressions, statements, and modules. We are only attaching environments to
-    statements, so expressions and modules are ignored; it makes no sense for a
-    module to have an environment, but the decision about expressions is a
-    matter of preference, and is chosen in the spirit of lazy evaluation (ie,
-    not assigning environment references all over the place when
-    possibly/probably unnecessary). For convenience, this function returns the
-    environment which is added to the node, or None if an environment isn't set.
-    """
-
-    # if tree is an expression.
-    if isinstance(tree, ast.expr) or isinstance(tree, ast.module):
-        # no environment will be added, so return None to signal this.
-        return None
-
-    # if tree is a statement
-    elif isinstance(tree, ast.stmt):
-        # we only need to add typedefs to the dictionary if it's a TypeDec statement
-        if tree.__class__.__name__ = "TypeDec":
-            new_env = old_env.copy()
-            
-            for target in tree.targets:
-                new_env[target.id] = tree.t
-                
-            tree.env = new_env
-        # but if it's any kind of statement with children, we need to recurse
-        # down and assign environments to those nodes.
+    def __init__(self, typed_tree, clone=False):
+        # make sure that it is initialized with a module AST node.
+        assert(typed_tree.__class__.__name__ == "Module")
+        
+        if clone:
+            self.tree = copy.deepcopy(typed_tree)
         else:
-            tree.env = old_env
+            self.tree = typed_tree
 
-            # need to get all the children which are lists of statements. AST's
-            # built-in function of iter_child_nodes won't quite do the job here,
-            # as it'll just return a generator with all the children nodes
-            # flattened.
-            stmt_lists = _get_stmt_lists(tree)
+        self._embed_environment()
 
-            for stmt_list in stmt_lists:
-                _embed_environment_stmt_list(stmt_list, old_env)
+    def _embed_environment(self):
+        """For each statement node of this L{EnvASTModule}'s underlying AST,
+        adds an instance variable called C{env} which is the type environment at
+        that stage of execution. 
+        """
 
-            # statements don't show up as children of other nodes (except as
-            # statement lists of course), and expressions don't get any
-            # environments added, so statement lists is all we really have to
-            # worry about
+        return _embed_environment_stmt_list(self.tree.body, {})
+
+    @staticmethod
+    def _embed_environment_node(node, old_env):
+        """Recursive helper for embed_environment to add environments to AST
+        nodes.  There are three distinct types of nodes that this can be called
+        on: expressions, statements, and modules. We are only attaching
+        environments to statements, so expressions and modules are ignored; it
+        makes no sense for a module to have an environment (also, a module
+        should not have module children, and this is only called on the children
+        of modules), but the decision about expressions is a matter of
+        preference, and is chosen in the spirit of lazy evaluation (ie, not
+        assigning environment references all over the place when
+        possibly/probably unnecessary). For convenience, this function returns
+        the environment which is added to the node, or None if an environment
+        isn't set.
+        """
+
+        if isinstance(node, ast.mod):
+
+            # this method should never be called when self.tree is a module.
+            assert(False)
             
-        return tree.env
+        elif isinstance(node, ast.expr):
 
-    # according to the Python API, AST nodes are disjoint sums of expressions,
-    # statements, and modules, so we should never reach here.
-    else:
-        assert(False)
-
-def _embed_environment_stmt_list(stmts, old_env):
-    """Recursive helper for embed_environment to add environments to lists of
-    statements. The list of statements to be processed is assumed to be
-    consecutively ordered within one block (and consisting of the entire block),
-    so we assume that the working environment of line n is the working
-    environment of line n-1, plus any type declarations that may have been added
-    in line n. We do not need to return or keep track of the environment at the
-    end of the list of statements because we are assuming that C{stmts} is the
-    entire contents of one block, so, once we're done with the block, we should
-    no longer know or care about the type declarations from within the block.
-
-    @returns: The environment at the end of the list of statements. This will
-    only be used when processing the list of statements in a module to get a
-    "master" environment for a file to use in debugging.
-    """
-
-    working_env = old_env
-    
-    for s in stmts:
-        working_env = _embed_environment_node(s, working_env)
-
-    return working_env
-
+            # no environment will be added, so return None to signal this.
+            return None
         
-def find_env(expr):
-    """This should only be called on an expression within an AST that has
-    already been propogated with environments. All statements will have
-    environments set as instance variables, but expressions will have to refer
-    to the environments of their parent statements. XXX This probably will be
-    redundant, because the environments of the parent statements will be
-    accessed and then typecheck will be called with those environments when the
-    children expressions are typechecked.
-    """
+        elif isinstance(node, ast.stmt):
 
-    # this function is pointless to call on anything other than expressions.
-    assert(isinstance(expr, ast.expr))
+            if is_typedec(node):
+                # if it's a typedec, then add typedefs to the dictionary.
+                
+                node.env = old_env.copy()
 
-    # TODO implement if necessary
+                for target in node.targets:
+                    node.env[target.id] = node.t
+
+                return node.env
+
+            elif is_simple_stmt(node):
+                # if it's a simple statement, but not a typedec, then the
+                # enviroment is the same as the previous statement's
+                # environment.
+
+                # NOTE for now, if the environment doesn't change between
+                # statements, the same env dictionary is being stored with a
+                # different reference; I don't think this should cause any
+                # issues for now, but we'll see.
+                node.env = old_env
+
+                return node.env
+
+            elif is_compound_stmt(node):
+                # if it's a compound statement, then add environments to the
+                # children statements, but we need to process each block
+                # differently so that variables declared in an if block aren't
+                # usable in the else block.
+
+                # TODO currently, the environment we store for a compound
+                # statement is just the environment of the typedec above it, and
+                # it has nothing to do with the typedecs in its statement lists,
+                # since a compound statement could have more than one statement
+                # list. in the future, it is planned for compound statements to
+                # store an environment for each statement list representing the
+                # type environment at the end of each statement list, and then
+                # instead of embedding several copies of environments at each
+                # statement making some kind of chain of references to
+                # environments so that data isn't copied and reused all over the
+                # place.
+                
+                stmt_lists = ASTInfo.get_stmt_lists(node)
+
+                for stmt_list in stmt_lists:
+                    _embed_environment_stmt_list(stmt_list, old_env)
+
+                return old_env
+
+            else:
+                # simple and compound statements should form a disjoint sum of
+                # all statements, so this should never be reached.
+                assert(False)
+
+        else:
+            # according to the Python API, AST nodes are disjoint sums of
+            # expressions, statements, and modules, so we should never reach
+            # here.
+            assert(False)
+
+    @staticmethod
+    def _embed_environment_stmt_list(stmts, old_env):
+        """Recursive helper for _embed_environment to add environments to lists
+        of statements. The list of statements to be processed is assumed to be
+        consecutively ordered within one block (and consisting of the entire
+        block), so we assume that the working environment of line n is the
+        working environment of line n-1, plus any type declarations that may
+        have been added in line n. We do not need to return or keep track of the
+        environment at the end of the list of statements because we are assuming
+        that C{stmts} is the entire contents of one block, so, once we're done
+        with the block, we should no longer know or care about the type
+        declarations from within the block.
+
+        @returns: The environment at the end of the list of statements. This will
+        only be used when processing the list of statements in a module to get a
+        "master" environment for a file to use in debugging.
+        """
+
+        current_env = old_env
+
+        for s in stmts:
+            current_env = _embed_environment_node(s, current_env)
+
+        return current_env
 
 
-    
-        
+    ## This probably won't be necessary.
+    ## def find_env(expr):
+    ##     """This should only be called on an expression within an AST that has
+    ##     already been propogated with environments. All statements will have
+    ##     environments set as instance variables, but expressions will have to refer
+    ##     to the environments of their parent statements. XXX This probably will be
+    ##     redundant, because the environments of the parent statements will be
+    ##     accessed and then typecheck will be called with those environments when the
+    ##     children expressions are typechecked.
+    ##     """
+
+    ##     # this function is pointless to call on anything other than expressions.
+    ##     assert(isinstance(expr, ast.expr))
+
+    ##     # TODO implement if necessary
+
+
+
+
