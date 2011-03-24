78193c5a02a35b552cddabe0a5b87e31c24cbeea
Jeff Ruberg <jruberg@wesleyan.edu>
2011-03-24 17:17:21 -0400

    got rid of DEBUG:root: messages and replaced with timestamps

--- src/logger.py
+++ src/logger.py
@@ -3,10 +3,15 @@ from datetime import datetime
 import logging
 from settings import *
 
-logging.basicConfig(level=LOG_LEVEL, filename=LOG_DIR+LOGFILE)
+logging.basicConfig(level=LOG_LEVEL, filename=LOG_DIR+LOGFILE,
+                    format='%(asctime)s: %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S')
+
+_MARGIN = len('%s, %s %s %s %s:%s:%s: ' %
+              ('Thu', '24', 'Mar', '2011', '17', '09', '37'))
 
 class Logger:
-    nl = "\n" + (" " * 12)
+    nl = "\n" + (" " * _MARGIN)
     
     def __init__(self):
         self.in_debug_file = False
@@ -19,8 +24,7 @@ class Logger:
 
     def debug(self, s, cond=True):
         if FILE_DEBUG and self.in_debug_file and cond:
-            logging.debug(" " + s.replace('\n', Logger.nl))
+            logging.debug(s.replace('\n', Logger.nl))
 
 def announce_file(filename):
-    logging.debug(" ---- RUNNING " + filename.upper() + " AT " +
-                  str(datetime.now()) + " ----" + Logger.nl*2)
+    logging.debug("--------- RUNNING " + filename.upper() + " ----------")
