ee470f105be7b65cd77199e183590524b30966e1
Jeff Ruberg <jruberg@wesleyan.edu>
2011-04-07 12:15:50 -0400

    changed naming scheme for types, added shorthand functions to match the output the parser should generate

--- src/pyty_types.py
+++ src/pyty_types.py
@@ -1,42 +1,86 @@
 import re
 from lepl import *
 
+class ListSpec(List):
+    def __init__(self, t):
+        self.t = t
+        return List(['list', t])
+
+    def typ(self):
+        return self.t
+
+class TupleSpec(List):
+    def __init__(self, ts):
+        self.ts = ts
+        lst = ['tuple']
+        lst.extend(ts)
+        return List(lst)
+
+    def typ(self, i):
+        return self.ts[i]
+
+    def typs(self):
+        return self.ts
+
+class DictSpec(List):
+    def __init__(self, key_t, val_t):
+        self.key_t = key_t
+        self.val_t = val_t
+        return List(['dict', key_t, val_t])
+
+    def key_typ(self):
+        return self.key_t
+
+    def val_typ(self):
+        return self.val_t
+
+class FuncSpec(List):
+    def __init__(self, par_t, ret_t):
+        self.par_t = par_t
+        self.ret_t = ret_t
+        return List(['fun', par_t, ret_t])
+
+    def par_typ(self):
+        return self.par_t
+
+    def ret_typ(self):
+        return self.ret_t
+
 class TypeSpecParser:
     int_tok = Token('int')
     float_tok = Token('float')
     bool_tok = Token('bool')
     str_tok = Token('str')
-    
-    list_tok = Token('list')
-    tuple_tok = Token('tuple')
-    dict_tok = Token('dict')
-    
-    of_tok = ~Token('of')
-    cross_tok= ~Token('\*')
-    col_tok = ~Token(':')
-    arrow_tok = ~Token('\->')
-    lpar_tok = ~Token('\(')
-    rpar_tok = ~Token('\)')
-    
+
+    list_start = Token('\[')
+    list_end = Token('\]')
+
+    tuple_start = Token('\(')
+    tuple_div = Token(',')
+    tuple_end = Token('\)')
+
+    dict_start = Token('\{')
+    dict_div = Token('\:')
+    dict_end = Token('\}')
+
+    fn_div = Token('\->')
+
     typ = Delayed()
-    
-    parens = lpar_tok & typ & rpar_tok
-    parens_typ = parens | typ
 
     base_typ = int_tok | float_tok | bool_tok | str_tok
 
-    lst = list_tok & of_tok & typ > List
+    lst = ~list_start & typ & ~list_end
 
     tup_comp = Delayed()
-    tup_comp += typ & Optional(cross_tok & tup_comp) > List
-    tup = tuple_tok & of_tok & tup_comp > List
-    dct_comp = typ & col_tok & typ > List
-    dct = dict_tok & of_tok & dct_comp > List
+    tup_comp += tup_div & typ & Optional(tup_comp)
+    tup = tup_start & typ & tup_comp & tup_end
 
-    fun = typ & arrow_tok & typ > List
+    dct = dict_start & typ & dict_div & dict_end
 
-    typ += base_typ | lst | tup | dct | fun | parens_typ
+    fun = tup & fn_div & typ
 
+    typ += base_type | lst | tup | dct | fun
+    
     @staticmethod
     def parse(s):
         return sexpr_to_tree(TypeSpecParser.typ.parse(s)[0])
--- test/pyty_types_unit_tests.py
+++ test/pyty_types_unit_tests.py
@@ -6,34 +6,80 @@ from pyty_types import *
 parse = TypeSpecParser.parse
 
 class TypeSpecTests(unittest.TestCase):
+    def test_list_spec(self):
+        self.assertEqual(ListSpec('int'), List(['list', 'int']))
+
+    def test_tuple_spec(self):
+        self.assertEqual(TupleSpec(['int', 'float']),
+                         List(['tuple', 'int', 'float']))
+
+    def test_dict_spec(self):
+        self.assertEqual(DictSpec('int', 'float'),
+                         List(['dict', 'int', 'float']))
+
+    def test_func_spec(self):
+        self.assertEqual(FuncSpec(TupleSpec(['int', 'int']), 'float'),
+                         List(['func',
+                               List(['tuple', 'int', 'int'])
+                               'float']))
+        self.assertEqual(FuncSpec(TupleSpec([]), 'str'),
+                         List(['func', List([]), 'str']))
+                         
+    
     def test_base_types(self):
-        self.assertEqual(parse('int'),   List(['int']))
-        self.assertEqual(parse('float'), List(['float']))
-        self.assertEqual(parse('bool'),  List(['bool']))
-        self.assertEqual(parse('str'),   List(['str']))
+        self.assertEqual(parse('int'),   'int')
+        self.assertEqual(parse('float'), 'float')
+        self.assertEqual(parse('bool'),  'bool')
+        self.assertEqual(parse('str'),   'str')
 
     def test_list(self):
-        self.assertEqual(parse('list of int'),
-                         List(['list', List(['int'])]))
-        self.assertEqual(parse('list of bool'),
-                         List(['list', List(['bool'])]))
-        self.assertEqual(parse('list of list of float'),
-                         List(['list', List(['list', List(['float'])])]))
-        self.assertEqual(parse('list of list of list of str'),
-                         List(['list',
-                               List(['list',
-                                     List(['list',
-                                           List(['str']) ]) ]) ]) )
+        self.assertEqual(parse('[int]'),
+                         ListSpec('int'))
+        self.assertEqual(parse('[ str ]'), parse('[str]'))
+        self.assertEqual(parse('[    float]'), parse('[float     ]'))
+        self.assertEqual(parse('[bool]'),
+                         ListSpec('bool'))
+        self.assertEqual(parse('[[float]]'),
+                         ListSpec(ListSpec('float')))
+        self.assertEqual(parse('[[[str]]]'),
+                         ListSpec(ListSpec(ListSpec('str'))))
 
     def test_tuple(self):
-        self.assertEqual(parse('tuple of int'),
-                         List(['tuple', List(['int'])]))
-        self.assertEqual(parse('tuple of bool * float'),
-                         List(['tuple', List(['bool', 'float'])]))
-        self.assertEqual(parse('tuple of int * str * float'),
-                         List(['tuple', List(['int', 'str', 'float'])]))
-        self.assertEqual(parse('tuple of list of int'),
-                         List(['tuple', List(['list', List(['int'])])])
+        self.assertEqual(parse('(int)'),
+                         TupleSpec(['int'])
+        self.assertEqual(parse('(  int )'), parse('(int)'))
+        self.assertEqual(parse('( bool,   str , float  , int )'),
+                         parse('(bool,str,float,int)'))
+        self.assertEqual(parse('(bool,str,float,int)'),
+                         parse('(bool, str, float, int)'))
+        self.assertEqual(parse('(bool, float)'),
+                         TupleSpec(['bool', 'float']))
+        self.assertEqual(parse('(int, str, float)'),
+                         TupleSpec(['int', 'str', 'float']))
+        self.assertEqual(parse('([int])'),
+                         TupleSpec([ListSpec('int')]))
+        self.assertEqual(parse('([bool], [str])'),
+                         TupleSpec([ListSpec('bool'), ListSpec('str')]))
+
+    def test_dict(self):
+        self.assertEqual(parse('{int: float}'), DictSpec('int', 'float'))
+        self.assertEqual(parse('{str: int}'), parse('{ str : int }'))
+        self.assertEqual(parse('{str: int}'), parse('{str:int}'))
+        self.assertEqual(parse('{str: [float]}'),
+                         DictSpec('str', ListSpec('float')))
+        self.assertEqual(parse('{float: (int, [str], bool)}'),
+                         DictSpec('float',
+                                  TupleSpec(['int', ListSpec('str'), 'bool'])))
+
+    def test_func(self):
+        self.assertEqual(parse('(int) -> float'),
+                         FuncSpec(TupleSpec(['int']), 'float'))
+        self.assertEqual(parse('( int )->float '), parse('(int) -> float'))
+        self.assertEqual(parse('() -> int')
+                         FuncSpec(TupleSpec([]), 'int'))
+        self.assertEqual(parse('([float]) -> int'),
+                         FuncSpec(TupleSpec([ListSpec('float')]), 'int'))
+                         
 
 if __name__ == '__main__':
     unittest.main
