5f2117ffc6f8ae6abef8913b15a3daf563fb9eab
jruberg <rubergly@gmail.com>
2011-10-17 12:52:53 -0400

    add inferring to cli.py

--- src/cli.py
+++ src/cli.py
@@ -1,22 +1,31 @@
 import ast
 
 from logger import Logger
-from typecheck import check_expr
+from typecheck import check_expr, infer_expr
 from parse_type import PytyType
 
 import typecheck
 
 log = typecheck.log = Logger()
+infer = False
 
 if __name__ == '__main__':
     while True:
-        expr = raw_input('Expression: ')
+        inp = raw_input(('Infer' if infer else 'Check') + ' expression: ')
 
-        if expr == 'q' or expr == 'quit':
+        if inp == 'i' or inp == 'infer':
+            infer = True
+            continue
+        elif inp == 'c' or inp == 'check':
+            infer = False
+            continue
+        elif inp == 'q' or inp == 'quit':
             break
         else:
-            e = ast.parse(expr).body[0].value
+            e = ast.parse(inp).body[0].value
 
-        t = PytyType(raw_input('Expected type: '))
-
-        print check_expr(e, t, {})
+        if infer:
+            print infer_expr(e, {})
+        else:
+            t = PytyType(raw_input('Expected type: '))
+            print check_expr(e, t, {})
--- test/spec/expr_subscript.spec
+++ test/spec/expr_subscript.spec
@@ -13,14 +13,19 @@ expr type: Subscript
 [1, 2.0, 3.0][0] : float
 [1, 2.0, 3.0][1] : float
 [1, 2.0, 3.0][5+3-2*2] : float
-[(True, False), (False, False), (True, True)][0] : (bool, bool)
-[(True, False), (False, False), (True, True)][0][1] : bool
-[(True, 4), (False, 1), (True, 9)][2][0] : bool
-[(True, 4), (False, 1), (True, 9)][2][1] : int
-[1, 2, 3][1.0] : int # this should typecheck, though it will not run
 (1,)[0] : int
 (1,)[0] : float
 (1.0,)[0] : float
+[True, True][0] : bool
+[[True]][0] : [bool]
+[(True,)][0] : (bool,)
+((True, True))[0] : (bool, bool)
+[(True, True)][0] : (bool, bool)
+# [(True, False), (False, False), (True, True)][0] : (bool, bool)
+# [(True, False), (False, False), (True, True)][0][1] : bool
+# [(True, 4), (False, 1), (True, 9)][2][0] : bool
+# [(True, 4), (False, 1), (True, 9)][2][1] : int
+
 
 ----fail----
 [1, 2.0, 3.0][0] : int
