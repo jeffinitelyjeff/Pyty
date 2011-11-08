1ce5d6c56d8b730eb8ffe3f5e02042bb97172980
jruberg <rubergly@gmail.com>
2011-11-08 01:52:53 -0500

    remove references to debugging env ast

--- test/unit_tests_core.py
+++ test/unit_tests_core.py
@@ -15,7 +15,7 @@ from parse_file import parse_type_decs
 from parse_type import PType
 from errors import TypeUnspecifiedError, TypeIncorrectlySpecifiedError
 from settings import (TEST_CODE_SUBDIR, DEBUG_SUBJECT_FILE, DEBUG_UNTYPED_AST,
-                      DEBUG_TYPED_AST, DEBUG_ENV_AST, DEBUG_TYPEDECS)
+                      DEBUG_TYPED_AST, DEBUG_TYPEDECS)
 from logger import Logger, announce_file
 
 # these should be redundant, but they're necessary to refer to the specific log
