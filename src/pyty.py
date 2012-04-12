9a69ffe24efc1ec740f818ab7b2f151742dfa05f
jruberg <rubergly@gmail.com>
2012-04-11 20:16:44 -0400

    implement checking interface and add inference interface

--- src/pyty.py
+++ src/pyty.py
@@ -5,6 +5,8 @@ from optparse import OptionParser, OptionGroup
 
 from logger import Logger
 from check import check_mod
+from infer import infer_expr
+from ptype import PType
 from parse_file import parse_type_decs
 from ast_extensions import TypeDecASTModule
 
@@ -16,28 +18,35 @@ log = check.log = parse_file.log = infer.log = Logger()
 # Invoked like:
 # python pyty.py <source_file.py>
 
-parser = OptionParser()
+usage = "usage: %prog [options]\n\nDo not mix options from different modes."
+
+parser = OptionParser(usage=usage)
 
 f_group = OptionGroup(parser, "File Mode",
-                      "Options to use Pyty to typecheck source code files. Do "
-                      "not use any Expression Mode options along with these.")
+                      "Use Pyty to typecheck source code files.")
 f_group.add_option("-f", "--file", dest="filename",
                    help="file to typecheck", metavar="FIL")
 parser.add_option_group(f_group)
 
 e_group = OptionGroup(parser, "Expression Mode",
-                      "Options to use Pyty to typecheck expressions (under the "
-                      "empty environment). Both options are required; do not use "
-                      "any File Mode options along with these.")
+                      "Use Pyty to typecheck expressions (under the "
+                      "empty environment). Both options are required.")
 e_group.add_option("-e", "--expr", dest="expr",
                    help="string of expression", metavar="EXP")
 e_group.add_option("-t", "--type", dest="type",
                    help="type to typecheck against", metavar="TYP")
 parser.add_option_group(e_group)
 
+i_group = OptionGroup(parser, "Inference Mode",
+                      "Use Pyty to infer the type of an expression (under the "
+                      "empty environment).")
+i_group.add_option("-i", "--inf", dest="infer_expr",
+                  help="string of expression", metavar="EXP")
+parser.add_option_group(i_group)
+
 (opt, args) = parser.parse_args()
 
-if opt.filename and not opt.expr and not opt.type:
+if opt.filename and not opt.expr and not opt.type and not opt.infer_expr:
     file_name = opt.filename
 
     try:
@@ -56,10 +65,18 @@ if opt.filename and not opt.expr and not opt.type:
     except IOError as e:
         print "File not found: %s" % e.filename
 
-elif opt.expr and opt.type and not opt.filename:
-    print "yay"
-    # FIXME
+elif opt.expr and opt.type and not opt.filename and not opt.infer_expr:
+    e = ast.parse(opt.expr).body[0].value
+    t = PType(opt.type)
+    template = ("YES! %s typechecks as type %s" if infer_expr(e, t, {}) else
+                "NO! %s does not typechecka s type %s")
+    print template % (opt.expr, opt.type)
 
+elif opt.infer_expr and not opt.filename and not opt.expr and not opt.type:
+    e = ast.parse(opt.infer_expr).body[0].value
+    print "%s -- is the inferred type of %s" % (infer_expr(e, {}),
+                                                opt.infer_expr)
+    
 else:
     parser.print_help()
 
